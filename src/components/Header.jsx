0ededc44707344fb4bb5f4f05ac48e902c711d1b
Ramon Holguin <ramon.holguin@autozone.com>
2024-02-29 10:11:09 -0600

    Modificacion de Header.jsx component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
       <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">Seguimiento Pacientes {''}
-        <span className="text-indigo-600">Veterinaria</span>
+        <span className="text-indigo-600">Veterinaria - Holguin</span>
       </h1>
 
     </>
